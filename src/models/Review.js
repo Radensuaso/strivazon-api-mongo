5fb4c35f4bd3c76fcc5ef413d713e27251b5bb6b
Marcelo Mantilla <marcelo@appcabinet.com>
2021-09-03 13:55:41 +0200

    Project Ready

---
+++ src/models/Review.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+import { Schema, model } from 'mongoose'
+
+const reviewSchema = new mongoose.Schema({
+	
+})
+
+const model = mongoose.model
+
+export default model('review', reviewSchema)
