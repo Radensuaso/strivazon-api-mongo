362ed5bbaa8a7f9ec25dcaa1be000a15417cdb35
Radensuaso <andrels9283@gmail.com>
2021-09-03 15:31:54 +0100

    Merge successfully done

--- src/services/users/index.js
+++ src/services/users/index.js
@@ -1,23 +1,23 @@
 import express from "express";
-import UserModel from "../../models/User.js"
-import createError from 'http-errors'
+import UserModel from "../../models/User.js";
+import createError from "http-errors";
 const router = express.Router();
 
 router
   .route("/")
   .get(async (req, res, next) => {
     try {
-    const users=await UserModel.find({})
-    res.send(users)
+      const users = await UserModel.find({});
+      res.send(users);
     } catch (error) {
       next(error);
     }
   })
   .post(async (req, res, next) => {
     try {
-      const user=new UserModel(req.body)
-      const {_id}=await user.save()
-      res.status(201).send({_id})
+      const newUser = new UserModel(req.body);
+      const user = await newUser.save();
+      res.status(201).send(user);
     } catch (error) {
       next(error);
     }
@@ -27,12 +27,11 @@ router
   .route("/:id")
   .get(async (req, res, next) => {
     try {
-      const user=await UserModel.findById(req.params.id)
-      if(user){
-        res.send(user)
-      }
-      else{
-        next(createError(404, `User with id ${req.params.id} not found!`))
+      const user = await UserModel.findById(req.params.id);
+      if (user) {
+        res.send(user);
+      } else {
+        next(createError(404, `User with id ${req.params.id} not found!`));
       }
     } catch (error) {
       next(error);
@@ -40,12 +39,15 @@ router
   })
   .put(async (req, res, next) => {
     try {
-      const modifiedUser=await UserModel.findByIdAndUpdate(req.params.id,req.body,{new:true})
-      if(modifiedUser){
-        res.send(modifiedUser)
-      }
-      else{
-        next(createError(404,`User with id ${req.params.id}not found`))
+      const modifiedUser = await UserModel.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+      );
+      if (modifiedUser) {
+        res.send(modifiedUser);
+      } else {
+        next(createError(404, `User with id ${req.params.id}not found`));
       }
     } catch (error) {
       next(error);
@@ -53,12 +55,13 @@ router
   })
   .delete(async (req, res, next) => {
     try {
-      const deletedUser=await UserModel.findByIdAndDelete(req.params.id,)
-      if(deletedUser){
-        res.status(204).send(`user with id ${req.params.id} deleted succesfully`)
-      }
-      else{
-        next(createError(404, `user with id ${req.params.id} not found!`))
+      const deletedUser = await UserModel.findByIdAndDelete(req.params.id);
+      if (deletedUser) {
+        res
+          .status(204)
+          .send(`user with id ${req.params.id} deleted succesfully`);
+      } else {
+        next(createError(404, `user with id ${req.params.id} not found!`));
       }
     } catch (error) {
       next(error);
