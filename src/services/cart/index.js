98d058ff7898169bb8cb137ee3e7aaf38a907b72
Marcelo Mantilla <marcelo@appcabinet.com>
2021-09-03 17:03:13 +0200

    Attempt 1

--- src/services/cart/index.js
+++ src/services/cart/index.js
@@ -33,15 +33,12 @@ router.route("/:userId").get(async (req, res, next) => {
 router.post('/:userId', async (req, res, next) => {
   try {
     const user = await userSchema.findById(req.params.userId)
-    console.log("USER:", user)
     
     if (user) {
-      
       const verifyCart = await cartSchema.find({user_id: req.params.userId})
-      console.log("CART EXISTS?:", verifyCart)
+      console.log("VERIFY CART:", verifyCart)
       
       if (verifyCart.length === 0 || verifyCart === null) {
-        console.log("DOESNT HAVE A CART")
         const createCart = await new cartSchema({
           user_id: req.params.userId,
           quantity: 0
@@ -51,26 +48,43 @@ router.post('/:userId', async (req, res, next) => {
       
       const product = await productSchema.findById(req.body.product_id)
       const { name, brand, price, imageUrl } = product
-      console.log("PRODUCT:", product)
       
-      const cart = await cartSchema.findOneAndUpdate({ user_id: user._id },
-        {
-          $push: { products: {
-            product_id: product._id,
-            name,
-            brand, 
-            price,
-            imageUrl,
-            quantity: req.body.quantity
-          }}
-        },
-        { 
-          new: true
-        }
-      )
-      cart.save()
-      console.log("Cart saved!✅")
-      res.send(cart)
+      const productExists = verifyCart.products.find(product => {
+        product.product_id.toString() == req.body.product_id
+      })
+      console.log("Product Exists?", productExists)
+      
+      if (productExists) {
+        const cart = await cartSchema.findOneAndUpdate({ user_id: user._id},
+          {
+            $inc: { "products.$.quantity": req.body.quantity }
+          },
+          { new: true }
+        )
+        console.log("Cart updated!✅")
+        res.send(cart)
+      } else {
+        const cart = await cartSchema.findOneAndUpdate({ user_id: user._id },
+          {
+            $push: { products: {
+              product_id: product._id,
+              name,
+              brand, 
+              price,
+              imageUrl,
+              quantity: req.body.quantity
+            }}
+          },
+          { 
+            new: true
+          }
+        )
+        console.log("Cart saved!✅")
+        res.send(cart)
+      }
+      
+      
+      
       
     } else {
       next(createError(400, "User does not exist."))
